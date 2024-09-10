876aa4887892b0ad2825710461b9f176bbbb1326
sheldonmetoyer <sheldon.metoyer@gmail.com>
2024-09-10 16:49:33 -0500

    first draft of ccpws
    All change from this point forward will be made
    and pushed to github

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const showButton = document.querySelector('.open-menu')
+const closeButton = document.querySelector('.close-button');
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex';
+
+}
+function closeSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none';
+
+}
+
+showButton.addEventListener('click', showSidebar);
+
+closeButton.addEventListener('click', closeSidebar);
